5bede1223aed4c145b8b38b696af0ca7b949db59
Lenddoo <146371681+Lenddoo@users.noreply.github.com>
2025-01-28 17:30:46 -0300

    footer

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<footer>
+    <div>
+        <img src="" alt="Camila">
+    </div>
+    <h2>R.Itapemirim, 436 - Jardim Ipiranga</h2>
+    <div>
+        <img src="assets\img\whatsapp.png" alt="whatsapp">
+        <img src="assets\img\instagram.png" alt="intagram">
+        <img src="assets\img\marcador.png" alt="local">
+    </div>
+</footer>
