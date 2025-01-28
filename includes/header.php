779b70a78fdaf286d13caa156dc569c8fa7eff5a
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-01-28 16:42:21 -0300

    desenvolvimento

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pi-Programação</title>
+    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="includes\footer.php">
+</head>
+<body class="body">
+    <div class="container">
+        <div class="img-logo">
+            <img id="logo" src="assets\img\img_logo_preto.png" alt="Logo">
+        </div>
+        <div class="img_login">
+            <img id="login" src="assets\img\img_login.png" alt="Login">
+        </div>
+
+    </div>
+</body>
+</html>
