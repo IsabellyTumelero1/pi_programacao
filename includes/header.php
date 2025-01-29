9bd752fc97c35f71933b7daa19ba6dfe799b6c23
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-01-29 08:53:47 -0300

    Logo e login, falta acertar algumas coisas

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,31 @@
     <link rel="stylesheet" href="assets\css\style.css">
     <link rel="stylesheet" href="includes\footer.php">
 </head>
+
 <body class="body">
-    <div class="container">
-        <div class="texto1">
-            <p>dfasfsafasfas</p>
+    <header>
+        <div class="container">
+            <div class="div-meio">
+                <img id="img-logo" src="assets\img\img_logo_preto.png" alt="img-logo">
+            </div>
+            <div class="div-direita">
+                <img id="img-login" src="assets\img\icons8-usuário-homem-com-círculo-32.png" alt="img-login">
+                <a href="#" class="texto-login">Entre ou Cadastre-se</a>
+            </div>
         </div>
 
-        <div class="texto2">
-            <p>dasfdasfdasfdasfasfas</p>
-        </div>
-    </div>
+        <nav>
+            <ul>
+                <li><a href="/">item 1</a></li>
+                <li><a href="/">item 2</a></li>
+                <li><a href="/">item 3</a></li>
+                <li><a href="/">item 4</a></li>
+                <li><a href="/">item 5</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
 </body>
+<script src="assets\js\script.js"></script>
+
 </html>
