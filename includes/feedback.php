ad18ec03712ee2c61ac3013afe80516d438eb7a0
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-02-07 16:47:51 -0300

    feedback

--- includes/feedback.php
+++ includes/feedback.php
@@ -1,14 +1,23 @@
-<div class="feedback">
+<section class="feedback">
     <h1>Feedback</h1>
-    <p>"Com certeza foi top de mais, dez da recepção, a pessoa que atendeu e o resultado maravilhoso. Muito obrigada, com toda certeza irei voltar sim"</p>
-    <p>"Uma profissional maravilhosa, sempre em atualização para trazer, para nós clientes, as novidades e melhoria sempre. Ambiente agradável, confortável e receptivo, daqueles que você chega e já pensa que quer ser cliente pra sempre. Nunca conheci nenhum profissional que mima tanto os clientes! Sucesso sempre!"</p>
-    <!-- <p>"Profissional incrível, espaço confortável e aconchegante, procedimentos de qualidade e ótimos resultados."</p>
-    <p>"O espaço é super aconchegante, cheio de detalhes que te fazem sentir especial. A Camila é uma querida e todos os procedimentos que já fiz com ela foram incríveis. Na minha sobrancelha só ela mexe, desde 2021!"</p>
-    <p>"Camila Santi Beauty excelente profissional, Além de ser uma pessoa incrível, super indico, amei o trabalho realizado em mim."
-    </p>
-    <p>"A Dra. Camila é uma ótima profissional. Amei o trabalho, o atendimento, o cuidado, o pós procedimento. Super indico."</p>
-    <p>"Experiência foi maravilhosa atendimento com excelente profissional.procedimento seguro e resultado acima do esperado! Adorei!"</p>
-    <p>"Eu amei todo o procedimento.
-Atendimento incrível, super atenciosa, apaixonada pelo que faz, explica tudo certinho e simplesmente amei e quero fazer de novo. Amo amo. Bjo gatona
-Parabéns! Vc é maravilhosa!"</p> -->
-</div>
+
+    <div class="feedback-container">
+        <div class="feedback-comentario">
+            <p>"Com certeza foi top de mais, dez da recepção, a pessoa que atendeu e o resultado maravilhoso. Muito obrigada, com toda certeza irei voltar sim"</p>
+        </div>
+        <div class="feedback-comentario">   
+            <p>"Uma profissional maravilhosa, sempre em atualização para trazer, para nós clientes, as novidades e melhoria sempre. Ambiente agradável, confortável e receptivo, daqueles que você chega e já pensa que quer ser cliente pra sempre. Nunca conheci nenhum profissional que mima tanto os clientes! Sucesso sempre!"</p>
+        </div>
+        <div class="feedback-comentario">
+            <p>"Profissional incrível, espaço confortável e aconchegante, procedimentos de qualidade e ótimos resultados."</p>
+        </div>
+        <!-- <p>"O espaço é super aconchegante, cheio de detalhes que te fazem sentir especial. A Camila é uma querida e todos os procedimentos que já fiz com ela foram incríveis. Na minha sobrancelha só ela mexe, desde 2021!"</p>
+        <p>"Camila Santi Beauty excelente profissional, Além de ser uma pessoa incrível, super indico, amei o trabalho realizado em mim."
+        </p>
+        <p>"A Dra. Camila é uma ótima profissional. Amei o trabalho, o atendimento, o cuidado, o pós procedimento. Super indico."</p>
+        <p>"Experiência foi maravilhosa atendimento com excelente profissional.procedimento seguro e resultado acima do esperado! Adorei!"</p>
+        <p>"Eu amei todo o procedimento.
+            Atendimento incrível, super atenciosa, apaixonada pelo que faz, explica tudo certinho e simplesmente amei e quero fazer de novo. Amo amo. Bjo gatona
+            Parabéns! Vc é maravilhosa!"</p> -->
+    </div>
+</section>
