545b6e12946b14b7e6c25cc1730df264962ebcc8
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-07 14:48:19 -0300

    Responsividade
    Arrumando a responsividade do menu

--- includes/header.php
+++ includes/header.php
@@ -9,26 +9,29 @@
     <link rel="stylesheet" href="assets\css\style.css">
 
 <body>
-    <header>
-        <a href="#"><img src="assets\img\img_logo_preto.png" alt="img-logo"></a>
-        <nav>
-            <ul class="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Quem somos</a></li>
-                <li><a href="#">Procedimentos</a>
-                    <ul class="dropdown">
-                        <li><a href="#">Procedimento1</a></li>
-                        <li><a href="#">Procedimento2</a></li>
-                        <li><a href="#">Procedimento3</a></li>
-                        <li><a href="#">Procedimento4</a></li>
-                        <li><a href="#">Procedimento5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Micro do Bem</a></li>
-                <li><a href="includes\login.php">Login</a></li>
-            </ul>
-        </nav>
-    </header>
+    <div class="header">
+        <header>
+            <a href="#"><img src="assets\img\img_logo_preto.png" alt="img-logo"></a>
+
+            <nav>
+                <ul class="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">Procedimentos</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Procedimento1</a></li>
+                            <li><a href="#">Procedimento2</a></li>
+                            <li><a href="#">Procedimento3</a></li>
+                            <li><a href="#">Procedimento4</a></li>
+                            <li><a href="#">Procedimento5</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Micro do Bem</a></li>
+                    <li><a href="includes\login.php">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
 </body>
 <script src="assets\js\script.js"></script>
 
