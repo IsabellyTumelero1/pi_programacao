5c0c8a45a9915f2204bce96a8ea6d1fe626e1d96
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-02-07 17:26:58 -0300

    desenvolvendo

--- includes/feedback.php
+++ includes/feedback.php
@@ -3,7 +3,7 @@
 
     <div class="feedback-container">
         <div class="feedback-comentario">
-            <p>"Com certeza foi top de mais, dez da recepção, a pessoa que atendeu e o resultado maravilhoso. Muito obrigada, com toda certeza irei voltar sim"</p>
+            <p>"Fiquei super impressionada, foi uma experiência incrível! A recepção foi excelente, com um atendimento atencioso e acolhedor desde o início. A pessoa que me atendeu foi muito profissional e me deixou à vontade. E o resultado… maravilhoso, superou minhas expectativas! Muito obrigada, com certeza retornarei e indicarei para todos. Até logo!"</p>
         </div>
         <div class="feedback-comentario">   
             <p>"Uma profissional maravilhosa, sempre em atualização para trazer, para nós clientes, as novidades e melhoria sempre. Ambiente agradável, confortável e receptivo, daqueles que você chega e já pensa que quer ser cliente pra sempre. Nunca conheci nenhum profissional que mima tanto os clientes! Sucesso sempre!"</p>
