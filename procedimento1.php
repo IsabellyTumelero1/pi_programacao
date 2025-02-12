382c3c93c4f5029be988d4677f07cc2d82cc8afb
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-12 17:21:51 -0300

    desenvolvimento

--- procedimento1.php
+++ procedimento1.php
@@ -15,9 +15,13 @@ include_once '.\includes\header.php';
         </div>
     </div>
 
-    <div class="h1">Técnica 1</div>
+    <div class="texto-procedimento">
 
-    
+        <h1 class="titulo-tecnica">Título</h1>
+        <p class="comentario-tecnica">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate doloremque voluptatum harum dignissimos, architecto, nostrum soluta voluptatem, neque modi consectetur quam aliquam. Tenetur sint accusantium est similique ipsam sequi velit, laborum porro id nemo rem dolore eum in facilis, nihil unde quas. Illum autem, eligendi tempore consequuntur nulla, excepturi ex, fugit cupiditate maxime quae enim. Ullam quidem dolor eos impedit. Repellendus quo qui debitis laudantium veniam fuga commodi voluptatem incidunt quibusdam mollitia enim id ipsa quod, totam amet dolores, aut reiciendis illo dolore repudiandae nam. Fuga, minus dolorem dolorum quos voluptatibus accusamus reiciendis numquam incidunt impedit veniam error eligendi non repellat eum nulla! Expedita suscipit inventore vero iusto laborum nam minima? In repudiandae soluta quae voluptates aperiam itaque deleniti possimus assumenda veritatis, praesentium reiciendis necessitatibus minima nam excepturi incidunt placeat quod, vitae dolorum! Assumenda magni eveniet quas perferendis eum accusantium esse aliquam, nemo ex totam. Expedita saepe numquam ipsa perspiciatis sed nobis, quae assumenda repellat, recusandae nisi dolore rem placeat vero. Aliquam repellat, quis cum blanditiis, modi soluta sit facilis incidunt accusantium perspiciatis sunt consequatur eaque officia est quaerat iusto commodi ducimus illo totam harum. Vel aliquid ipsam accusantium, nam qui exercitationem minus atque molestiae beatae sint similique sed asperiores provident pariatur, maxime voluptatem esse! Ut quos rem, eaque itaque reprehenderit a esse delectus magni! Id, repellendus harum doloremque itaque illum inventore voluptatum expedita soluta, temporibus hic earum nisi aspernatur illo accusamus quos totam nesciunt placeat similique doloribus corporis voluptas laboriosam laborum voluptatibus! Enim inventore illum non perspiciatis. Tempore, amet!</p>
+    </div>
+
+
 </section>
 
 <?php
