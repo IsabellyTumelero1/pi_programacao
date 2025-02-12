b2d2157d38a31bacf8ee5ea5083cd82f64c4a7c3
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-02-12 16:47:52 -0300

    desenvolvimento

--- procedimento1.php
+++ procedimento1.php
@@ -1,11 +1,25 @@
-<?php 
-    include_once '.\includes\header.php';
+<link rel="stylesheet" href=".\assets\css\procedimento.css">
+
+<?php
+include_once '.\includes\header.php';
 ?>
 
 <section>
-    <h1>Olá Mundo</h1>
+    <div class="container2">
+        <h1 class="titu">Técnica 1</h1>
+
+        <div class="img-tecnica">
+        </div>
+        <br>
+        <div class="img-tecnica">
+        </div>
+
+        <div class="texto">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nesciunt earum, eveniet facilis ea dolorum pariatur tempora distinctio et repellendus. Itaque fugiat atque veritatis a, reprehenderit, sunt quo accusantium tenetur iste quod excepturi pariatur quaerat sapiente dolorem esse. Ut nam totam qui ad quaerat ex ipsam accusamus mollitia doloribus, eius, cupiditate sunt debitis error inventore dicta atque modi aut sed illum vitae itaque molestias. Quos fuga quo porro magni fugiat, eaque explicabo, tenetur dicta omnis, cum commodi minus. Eveniet molestias cupiditate voluptate rem enim nesciunt quia iusto quis perferendis omnis, repellat et dolorum fuga laboriosam, laborum nam, vel eligendi. Incidunt nihil minima laudantium quaerat doloremque rerum labore animi dolorem, nostrum, officia quae? Quod quia nobis nisi minima amet est ex earum maxime, ab vero pariatur nulla rem molestias mollitia quaerat corporis animi sit fugiat cumque reprehenderit a quidem harum, deserunt inventore! Natus quo, culpa aliquid obcaecati iusto, earum molestiae cumque itaque sequi dolorem, minus vitae tempore cupiditate adipisci modi doloribus perspiciatis eum quia consequuntur velit libero. Hic nisi, optio totam suscipit omnis mollitia itaque placeat eos quia iste consequuntur architecto asperiores voluptatem sunt nemo iusto blanditiis distinctio molestias illum, aliquid, aut culpa. Architecto fugiat dicta natus dolores dolore repellat quibusdam repellendus? Reprehenderit, eos beatae laudantium laborum consequuntur quae maiores? Consequuntur aspernatur ipsum consequatur! Aliquid ea sit hic totam? Quae obcaecati corporis veritatis molestiae! Ullam nulla perferendis corporis quidem incidunt, nihil iusto provident dolorem eius repellendus quas reiciendis officiis, fugit, in suscipit dicta ducimus accusamus magnam aliquam quo! Cum sequi in distinctio? Iure reprehenderit odio, voluptate quaerat quis nihil repellendus sint similique et aperiam, sit illo officiis accusantium obcaecati tempore enim exercitationem blanditiis sapiente rerum quia veniam sed hic, est doloremque. Ipsum est numquam mollitia nihil. Quidem similique ab, quos veritatis blanditiis ea. Doloremque harum rem iure architecto accusantium corrupti nostrum!</p>
+        </div>
+    </div>
 </section>
 
 <?php
-    include '.\includes\footer.php';
+include '.\includes\footer.php';
 ?>
