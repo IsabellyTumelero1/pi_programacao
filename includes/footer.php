887d855ab53756158e201965b15d14787fe538b8
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-02-19 14:06:40 -0300

    atualizando

--- includes/footer.php
+++ includes/footer.php
@@ -6,9 +6,9 @@
         <p class="endereco">R.Itapemirim, 436 - Jardim Ipiranga</p>
 
         <div class="redes-sociais">
-            <img class="imagem" src="assets\img\whatsapp.png" alt="">
-            <img class="imagem" src="assets\img\instagram.png" alt="">
-            <img class="imagem" src="assets\img\marcador.png" alt="">
+            <a href=""><img class="imagem" src="assets\img\whatsapp.png" alt=""></a>
+            <a href="https://www.instagram.com/bycamilasanti/" target="blank"><img class="imagem" src="assets\img\instagram.png" alt=""></a>
+            <a href="https://www.google.com/maps/place/Camila+Santi+Beauty/@-22.7485225,-47.3586831,16.5z/data=!4m6!3m5!1s0x94c89bb600c4c543:0x3d6d6e560cab4005!8m2!3d-22.7475626!4d-47.3580828!16s%2Fg%2F11fvwrjz43?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D" target="_blank"><img class="imagem" src="assets\img\marcador.png" alt=""></a>
         </div>
     </div>
 
