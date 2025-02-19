0541c4bb0adf8cd734f9d32309572fdb43ec389c
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-19 16:50:57 -0300

    desenvolvimento

---
+++ procedimentos-nanoLips.php
@@ -0,0 +1,34 @@
+<?php
+include '.\includes\header.php';
+?>
+
+<section class="Tecnica">
+    <h1 class="titulo-nanoLips">Técnica 1</h1>
+    <div class="conteudo-tecnica">
+        <div class="image-nanoLips">
+            <img class="img-nanoLips-formatacao" src="assets\img\img-nanoLips.jpg" alt="Imagem_1">
+            <img class="img-nanoLips-formatacao" src="assets\img\img-nanoLips2.jpg" alt="Imagem_2">
+        </div>
+        <p class="descricao-nanoLips">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure in velit, voluptas sit dicta fugit sunt
+            laboriosam recusandae doloribus odio magnam. Id, dolor architecto veniam et praesentium odio, atque
+            vitae inventore in hic tempore adipisci nam voluptatibus iste itaque recusandae illum molestias sit,
+            deleniti rem repellendus placeat! Necessitatibus, quisquam maiores laboriosam ullam nulla deleniti
+            laudantium aspernatur autem voluptas accusamus nisi illo sed tempora ea a pariatur consequatur harum in
+            ipsum vero totam exercitationem alias consequuntur? Laborum asperiores, maiores dignissimos dolores odio
+            ut! Minus recusandae, non error molestias vero culpa itaque esse consequatur veniam ab magnam commodi
+            maiores, tenetur molestiae quidem magni distinctio adipisci quo nobis aspernatur dolore animi voluptatem
+            quos. Exercitationem consectetur velit nisi itaque. Aperiam, veritatis, repudiandae exercitationem animi
+            eveniet fugit velit eaque aut dolorem voluptas impedit officiis. Laudantium iste eum commodi excepturi
+            aspernatur debitis voluptate ipsa minus aperiam, nisi quis amet magni porro sed tempora totam. Veritatis
+            necessitatibus deleniti sapiente deserunt qui rem reprehenderit ipsam pariatur quaerat culpa magni
+            voluptas cumque accusamus tempore iste velit illum ea, totam voluptatem doloribus saepe quo maiores esse
+            adipisci. Quam fuga fugit quidem? Illo ab magnam ipsum sunt, quidem iste veritatis porro, facilis
+            aliquid repellendus culpa sequi eligendi, similique nam quia cum!
+            <br><br>
+            <button class="btn-agendamento"><a class="btn-link" href="login.html">Agende-se</a></button>
+        </p>
+    </div>
+</section>
+<?php
+include '.\includes\footer.php';
+?>
