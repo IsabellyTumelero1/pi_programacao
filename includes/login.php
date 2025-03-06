b43f3c2a9a367d317c61b9d58109f1a7843ef02e
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-06 16:08:26 -0300

    Adicionando link para o whatsapp

--- includes/login.php
+++ includes/login.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets\img\favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         :root {
