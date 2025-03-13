ca117b64612b1d9b4bdf9bb5511e9fe22534243a
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-03-13 16:43:26 -0300

    ajustes nos links do footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
         <p class="endereco">R.Itapemirim, 436 - Jardim Ipiranga</p>
 
         <div class="redes-sociais">
-            <a href="https://web.whatsapp.com/"><img class="imagem" src="assets\img\whatsapp.png" alt=""></a>
+            <a href="https://wa.me/5519981758558" target="blank"><img class="imagem" src="assets\img\whatsapp.png" alt=""></a>
             <a href="https://www.instagram.com/bycamilasanti/" target="blank"><img class="imagem" src="assets\img\instagram.png" alt=""></a>
             <a href="https://www.google.com/maps/place/Camila+Santi+Beauty/@-22.7485225,-47.3586831,16.5z/data=!4m6!3m5!1s0x94c89bb600c4c543:0x3d6d6e560cab4005!8m2!3d-22.7475626!4d-47.3580828!16s%2Fg%2F11fvwrjz43?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D" target="_blank"><img class="imagem" src="assets\img\marcador.png" alt=""></a>
         </div>
