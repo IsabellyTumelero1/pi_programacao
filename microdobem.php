d28968cb50de098146a4632c152ad7a3aed30990
Lenddoo <146371681+Lenddoo@users.noreply.github.com>
2025-03-13 16:53:21 -0300

    procedimentos do Micro do Bem

--- microdobem.php
+++ microdobem.php
@@ -28,8 +28,7 @@
         }
         .txtInicio-doBem{
             border-top: 1px solid black;
-            border-right: 1px solid black;
-            
+            border-right: 1px solid black;   
         }
         .txt-doBem{
             text-align: left; 
@@ -44,16 +43,57 @@
             margin-top: 100px;
         }
        
+        .flex-doBem{
+            display: flex;
+        }
+
+         
+        section.micro-do-bem2{
+            background-color: black;
+            padding: 80px 10%;
+            font-family: 'Roboto', sans-serif;
+        }
+        .prcedimento-doBem{
+            display: flex; 
+            gap: 100px; 
+        } 
+        .txtProcedimento-doBem{
+            border-left: 1px solid var(--color-rosa-escuro);
+            border-bottom: 1px solid var(--color-rosa-escuro);
+        }
+        .txtProcedimento-doBem h2{
+            font-size: 38px;
+            letter-spacing: -0.5px;
+            color: var(--color-rosa-escuro);
+            margin-left: 7%;
+            margin-top: 70px;
+        }
+        .txt-doBem2{
+            text-align: left;
+            color: var(--color-rosa-escuro);
+            margin-left: 10%;
+            margin-right: 10%;
+            margin-bottom: 80px;
+        }
+        .img-microdobem{
+            margin-top: 150px;
+            margin-right: 40px;
+        }
+        .img-procedimento-doBem{
+            border-right: 1px solid var(--color-rosa-escuro);
+            border-top: 1px solid var(--color-rosa-escuro);
+        }
+
     </style>
     <section class="micro-do-bem">
         <div class="flex-doBem">
             <div class="OQueE-MicroDoBem">
-
                 <div class="img-inicio-dobem" style="min-height: 500px; max-height: 500px; min-width: 300px; max-width: 300px; border-radius: 15px; background-color: red;">
-                    <img src="" alt="">
+                    <!--trocar para a image definitiva-->
+                    <img src="">
                 </div>
                 <div class="txtInicio-doBem">
-                    <h2 class="">Micro do Bem</h2>
+                    <h2 >Micro do Bem</h2>
                     <div class="txt-doBem">
                     <p>
                         Transformar a dor em esperança através da micropigmentação paramédica tem sido um propósito que me acompanha há alguns anos. 
@@ -64,13 +104,40 @@
                         Esse trabalho vai além da estética, é um verdadeiro recomeço, permitindo que essas mulheres se vejam e se sintam novamente completas e confiantes.
                     </p>
                     </div>
-                </div>
-                <div>
-                    <div>
-
+                </div>   
+            </div>
+        </div>
+    </section>
+    <section class="micro-do-bem2">
+        <div class="flex-doBem">
+            <div class="prcedimento-doBem">
+                <div class="txtProcedimento-doBem">
+                    <h2>
+                        Procedimentos
+                    </h2>
+                    <div class="txt-doBem2">
+                        <p>
+                            a mastectomia (cirurgia para remoção da mama). Diferente de uma tatuagem tradicional, este procedimento utiliza equipamentos e tintas desenvolvidas especialmente para esse tipo de trabalho, que são mais suaves e biocompatíveis com a pele.
+                        </p>
+                        <p><br>
+                            O objetivo é criar uma aparência natural e harmônica, tanto em termos de cor quanto de textura, para que a reconstrução das aréolas seja o mais realista possível. Além de devolver a simetria e a beleza da região, a micropigmentação paramédica desempenha um papel importante na restauração da autoestima, promovendo uma nova percepção do corpo.
+                        </p>
+                        <p><br>
+                            Dependendo da complexidade de cada caso e da reação da pele, o procedimento pode ser realizado em uma ou mais sessões, sempre com o cuidado e a atenção necessários para garantir um resultado perfeito.
+                        </p>
                     </div>
                 </div>
-     
+                <div class="img-procedimento-doBem">
+                    <!--trocar para a image definitiva-->
+                    <img class="img-microdobem" src="https://s3-alpha-sig.figma.com/img/dbe1/0a2a/cb2565b13f4ec2110eb3e67fac06e7c2?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=W6YGfFhi9KYc5Uy9oHxF96nLo3rAUKDEJF10nlWpUWWENAC4Fib0lwz18NDPMTTeHklLpDKUBvx33pYHhU0rP6e~V6huCzk3tcHJcuwYVzrl42V2S2DhRPESiokBkgz9LJmDRbA9MYZXusEC8OI~rynVvI-r09wvPLhf~Ito88-qtYxNb1LnclG5Qz1qwMay0lYbAS27LY1jTOpCCc3XVDpgjQwEBzq0dHGzH4YJBGK-sY4p~IB4hP8Mh~hzFPc2fJmNvzw3wKzBKrjEngBBfLSm8vSbEMYxau-I4C4SJ6K6p9hwZ2aqQvnv5HKRN4W64GJuV7tI32pCiHKFmsnD0Q__">
+                </div>
+            </div>
+        </div>                
+    </section>
+    <section class="">
+        <div class="flex-doBem">
+            <div class="">
+
             </div>
         </div>
     </section>
