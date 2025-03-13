85da80752c2c780d324addfec8770d1a8cb1df7d
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-13 17:12:10 -0300

    Update agenda.php
    Adicionando o favicon para a página.

--- includes/agenda.php
+++ includes/agenda.php
@@ -27,7 +27,7 @@ $result = $conexao->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
-    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
