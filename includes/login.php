6d1883bdbb001d337bba5084406e3cc93f9b0827
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-13 15:39:51 -0300

    Update login.php

--- includes/login.php
+++ includes/login.php
@@ -131,4 +131,5 @@ if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
