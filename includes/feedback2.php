3315c9a0bd1fde064d237acf65d265b84b5b8b22
Lenddoo <146371681+Lenddoo@users.noreply.github.com>
2025-03-18 15:00:56 -0300

    fim do vão do feedbck

---
+++ includes/feedback2.php
@@ -0,0 +1,30 @@
+<section class="feedback">
+    <h1 class="h2-doBem">Feedback</h1>
+    <div class="feedback-container">
+        <div class="feedback-comentario">
+            <p>
+                Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+
+            </p><br>
+            <p class="nome">
+                - Mussum1
+            </p>
+        </div>
+        <div class="feedback-comentario">
+            <p>
+                Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+            </p>
+            <p class="nome">
+                - Mussum2
+            </p>
+        </div>
+        <div class="feedback-comentario">
+            <p>
+                Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+            </p>
+            <p class="nome">
+                - Mussum3
+            </p>
+        </div>
+    </div>
+</section>
--- microdobem.php
+++ microdobem.php
@@ -102,14 +102,9 @@
             border-top: 1px solid var(--color-rosa-escuro);
         }
 
-        /* .h2-doBem {
-            text-align: center;
-            margin-bottom: 10px;
-            padding: 20px;
-            margin-bottom: -100px;
-            color: var(--color-black);
-            font-size: 35px;
-        } */
+        .h2-doBem {
+            margin: 0;
+        }
 
         
 
@@ -163,36 +158,7 @@
             </div>
         </div>
     </section>
-    <section class="feedback">
-        <h1 class="h2-doBem">Feedback</h1>  
-        <div class="feedback-container">  
-            <div class="feedback-comentario">
-                <p>
-                    Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
-
-                </p><br>
-                <p class="nome">
-                    - Mussum1
-                </p>
-            </div>
-            <div class="feedback-comentario">
-                <p>
-                    Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
-                </p>
-                <p class="nome">
-                    - Mussum2
-                </p>
-            </div>
-            <div class="feedback-comentario">
-                <p>
-                    Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
-                </p>
-                <p class="nome">
-                    - Mussum3
-                </p>
-        </div>
-            
-    </section>
+    <?php include '.\includes\feedback2.php'?>
 </body>
 <?php include '.\includes\footer.php'; ?>
 
