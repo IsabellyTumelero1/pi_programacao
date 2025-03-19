0a3a1b214a58654e9a62b105a6ccd72bac8b41b5
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-19 14:09:09 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".next")?.addEventListener("click", nextSlide);
 
     // Troca automática de slides a cada 10 segundos
-    setInterval(nextSlide, 5000);
+    setInterval(nextSlide, 10000);
 
     // Atualiza a posição inicial do carrossel
     updateCarrossel();
