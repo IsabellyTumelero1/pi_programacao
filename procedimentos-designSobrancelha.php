4b97a95d7a79b7c62c45ba9080e2a6ff2dd8954d
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-20 14:58:04 -0300

    Desenvolvimento

--- procedimentos-designSobrancelha.php
+++ procedimentos-designSobrancelha.php
@@ -13,21 +13,7 @@ include '.\includes\header.php';
 
         <div class="texto-tecnica">
             <div class="titulo-tecnica">Design de Sobrancelha</div>
-            <p class="conteudo-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure in velit, voluptas sit dicta fugit sunt
-                laboriosam recusandae doloribus odio magnam. Id, dolor architecto veniam et praesentium odio, atque
-                vitae inventore in hic tempore adipisci nam voluptatibus iste itaque recusandae illum molestias sit,
-                deleniti rem repellendus placeat! Necessitatibus, quisquam maiores laboriosam ullam nulla deleniti
-                laudantium aspernatur autem voluptas accusamus nisi illo sed tempora ea a pariatur consequatur harum in
-                ipsum vero totam exercitationem alias consequuntur? Laborum asperiores, maiores dignissimos dolores odio
-                ut! Minus recusandae, non error molestias vero culpa itaque esse consequatur veniam ab magnam commodi
-                maiores, tenetur molestiae quidem magni distinctio adipisci quo nobis aspernatur dolore animi voluptatem
-                quos. Exercitationem consectetur velit nisi itaque. Aperiam, veritatis, repudiandae exercitationem animi
-                eveniet fugit velit eaque aut dolorem voluptas impedit officiis. Laudantium iste eum commodi excepturi
-                aspernatur debitis voluptate ipsa minus aperiam, nisi quis amet magni porro sed tempora totam. Veritatis
-                necessitatibus deleniti sapiente deserunt qui rem reprehenderit ipsam pariatur quaerat culpa magni
-                voluptas cumque accusamus tempore iste velit illum ea, totam voluptatem doloribus saepe quo maiores esse
-                adipisci. Quam fuga fugit quidem? Illo ab magnam ipsum sunt, quidem iste veritatis porro, facilis
-                aliquid repellendus culpa sequi eligendi, similique nam quia cum!
+            <p class="conteudo-descricao">Suas sobrancelhas são a moldura do seu rosto, e um design bem feito faz toda a diferença na sua expressão e beleza natural! O design de sobrancelhas é um procedimento essencial para alinhar, modelar e realçar o olhar, respeitando o formato do seu rosto e suas características únicas. Com técnicas precisas e modernas, removemos excessos, corrigimos assimetrias e criamos um desenho harmonioso, garantindo um acabamento impecável. Utilizamos métodos seguros e indolores para proporcionar um resultado sofisticado e natural, sem comprometer a saúde dos fios. Além disso, o design pode ser combinado com técnicas como a henna para um efeito mais preenchido e duradouro. Perfeito para quem deseja um olhar mais marcante, equilibrado e expressivo, sem precisar recorrer à maquiagem todos os dias. Venha transformar seu visual com um design de sobrancelhas personalizado e realce sua beleza de forma elegante e natural! Agende já o seu atendimento.
                 <br><br>
                 <button class="btn-agendamento">
                     <?php
@@ -37,7 +23,8 @@ include '.\includes\header.php';
                         echo '<a class="btn-link" href="includes/agendamento.php">Agende-se</a>';
                     } else {
                         // Se não estiver logado, redireciona para a página de login
-                        echo '<a class="btn-link" href="includes/login.php">Faça login para agendar</a>';
+                        echo '<a class="btn-link" href="includes/login.php">Logar</a>';
+                        echo '<small style="color: red;">* Login necessário para agendamento</small>';
                     }
                     ?>
                 </button>
